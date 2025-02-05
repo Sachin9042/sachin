9c48f98c56e73b3b0c1e751e1d2c5ad1c7ce5eaa
Sachin <sachinsivpm100@gmail.com>
2025-02-05 13:47:29 +0530

    Add files via upload

---
+++ arithmatic.js
@@ -0,0 +1,50 @@
+// let a = 50;
+// let b = 20;
+
+// c = a+b;
+// d = a-b;
+// f = a*b;
+// g = a/b;
+// h = a%b;
+// i = a**2;
+// console.log("add = ",c);
+// console.log("sub = ",d);
+// console.log("product = ",f);
+// console.log("divide = ",g);
+// console.log("remaider = ",h);
+// console.log("power = ",i);
+
+// let a=10;
+// b=20;
+// c=30;
+// d=40;
+
+// post_increment= a++;
+// pre_increment= ++b;
+// post_decrement= a--;
+// pre_decrement= --b;
+
+// console.log(post_increment);
+// console.log(pre_increment);
+// console.log(post_decrement);
+// console.log(pre_decrement);
+
+
+// function myfun() {
+// console.log("hello, buddy");    
+// }
+
+// myfun();
+
+// function hi(name){
+//     console.log("hi,",name);
+// }
+
+// hi("kavin");
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+
+// add(10,20);
+
